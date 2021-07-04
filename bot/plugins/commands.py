d44e41a8c20b80569dce1f837de78d104268cc63
Klfilms <86608269+Klfilms@users.noreply.github.com>
2021-07-04 14:07:27 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'KL_FILMS', url="https://t.me/filmsnew169"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'KL_FILMS', url="https://t.me/filmsnew169"
                                 )
                         ]
                     ]
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'KL_FILMS', url="https://t.me/CrazyBotsz"
                                 )
                         ]
                     ]
@@ -89,10 +89,8 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
-    ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Developers', url='https://t.me/filmsnew169'),
+        
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
